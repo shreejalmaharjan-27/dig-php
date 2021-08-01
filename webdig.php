d1c1b0d2ae699dac50b174be74361c970c08998b
Shreejal Maharjan <shreejal.m123@hotmail.com>
2021-08-01 23:41:01 +0545

    updates

--- webdig.php
+++ webdig.php
@@ -1,15 +1,21 @@
 <?php
 //get domain query
 $domain_inp = $_GET["domain"];
+$port = $_GET["port"];
 $options_pre1 = $_GET["opt1"];
 $option_pre2 = $_GET["opt2"];
 $options1 = htmlspecialchars($options_pre1, ENT_QUOTES);
 $options2 = htmlspecialchars($options_pre2, ENT_QUOTES);
 $domain = htmlspecialchars($domain_inp, ENT_QUOTES);
 if (preg_match("/^[.A-Za-z0-9-]+$/", $domain)) {
-    //execute shell command
-    $shellexec = shell_exec("dig $options1 $domain $options2");
-    echo '<pre>'.$shellexec.'</pre>';
+    if($port < 1 or $port > 65535){
+         print $x. 'is invalid port';
+    }
+    else{
+            //execute shell command
+        $shellexec = shell_exec("dig -p $port $options1 $domain $options2");
+        echo '<pre>'.$shellexec.'</pre>';
+    }
 } else {
     // does not contain only letters, numbers, and hyphens
     echo 'Invalid domain name provided!';
