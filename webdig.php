c90d758b692372a131f69153d4467a7e8d1d23ea
Shreejal Maharjan <shreejal.m123@hotmail.com>
2021-08-01 23:05:05 +0545

    updates

--- webdig.php
+++ webdig.php
@@ -4,7 +4,7 @@ $domain_inp = $_GET["domain"];
 $domain = htmlspecialchars($domain_inp, ENT_QUOTES);
 if (preg_match("/^[.A-Za-z0-9-]+$/", $domain)) {
     //execute shell command
-    $shellexec = shell_exec("dig $domain");
+    $shellexec = shell_exec("dig $domain ANY");
     echo '<pre>'.$shellexec.'</pre>';
 } else {
     // does not contain only letters, numbers, and hyphens
