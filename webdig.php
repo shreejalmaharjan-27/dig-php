baba08eb387795a9c17acd6075be7f8bdaab0b12
Shreejal Maharjan <shreejal.m123@hotmail.com>
2021-08-01 23:33:07 +0545

    updates

--- webdig.php
+++ webdig.php
@@ -3,7 +3,7 @@
 $domain_inp = $_GET["domain"];
 $options_pre1 = $_GET["opt1"];
 $option_pre2 = $_GET["opt2"];
-$options1 = htmlspecialchars($options_pre1, ENT_QUOTES);4
+$options1 = htmlspecialchars($options_pre1, ENT_QUOTES);
 $options2 = htmlspecialchars($options_pre2, ENT_QUOTES);
 $domain = htmlspecialchars($domain_inp, ENT_QUOTES);
 if (preg_match("/^[.A-Za-z0-9-]+$/", $domain)) {
