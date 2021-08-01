21d6e802f57aa9e47254e866dec859d522af1adf
Shreejal Maharjan <shreejal.m123@hotmail.com>
2021-08-01 23:14:54 +0545

    updates

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 <body>
 <form action="webdig.php" method="GET">
 <input required name="domain" placeholder="enter domain name">
+<select name="opt">
+<option value="" disabled selected>Choose option</option>
+<option value="+nocomments +noquestion +noauthority +noadditional +nostats">Answer Only</option>
+</select>
 <button type="submit">submit</button>
 </form>
 </body>
--- webdig.php
+++ webdig.php
@@ -1,10 +1,12 @@
 <?php
 //get domain query
 $domain_inp = $_GET["domain"];
+$options_pre = $_GET["opt"];
+$options = htmlspecialchars($options_pre, ENT_QUOTES);
 $domain = htmlspecialchars($domain_inp, ENT_QUOTES);
 if (preg_match("/^[.A-Za-z0-9-]+$/", $domain)) {
     //execute shell command
-    $shellexec = shell_exec("dig $domain ANY");
+    $shellexec = shell_exec("dig $domain $options");
     echo '<pre>'.$shellexec.'</pre>';
 } else {
     // does not contain only letters, numbers, and hyphens
