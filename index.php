e173b85a68bfb92b8fdf6f1c77bdb9a555be1cef
Shreejal Maharjan <shreejal.m123@hotmail.com>
2021-08-01 23:31:44 +0545

    updates

--- index.php
+++ index.php
@@ -5,12 +5,18 @@
 <body>
 <form action="webdig.php" method="GET">
 <input required name="domain" placeholder="enter domain name">
-<select name="opt">
-<option value="" disabled selected>Choose option</option>
+<select name="opt1">
+<option value="" disabled selected>Choose connect method</option>
+<option value="-4">IPV4</option>
+<option value="-6">IPV6</option>
+</select>
+<select name="opt2">
+<option value="" disabled selected>Choose records to get</option>
 <option value="ANY +noall +answer">Get All records</option>
 <option value="+nocomments +noquestion +noauthority +noadditional +nostats">Get Answer Only</option>
 <option value="MX +noall +answer">Get MX records</option>
 <option value="NS +noall +answer">Get NS records</option>
+<option value="+nssearch">Get SOA records </option>
 <option value="mx +short">Get Short MX records</option>
 <option value="ns +short">Get Short NS records</option>
 <option value="+short">Get Short Output</option>
--- webdig.php
+++ webdig.php
@@ -1,12 +1,14 @@
 <?php
 //get domain query
 $domain_inp = $_GET["domain"];
-$options_pre = $_GET["opt"];
-$options = htmlspecialchars($options_pre, ENT_QUOTES);
+$options_pre1 = $_GET["opt1"];
+$option_pre2 = $_GET["opt2"];
+$options1 = htmlspecialchars($options_pre1, ENT_QUOTES);4
+$options2 = htmlspecialchars($options_pre2, ENT_QUOTES);
 $domain = htmlspecialchars($domain_inp, ENT_QUOTES);
 if (preg_match("/^[.A-Za-z0-9-]+$/", $domain)) {
     //execute shell command
-    $shellexec = shell_exec("dig $domain $options");
+    $shellexec = shell_exec("dig $options1 $domain $options2");
     echo '<pre>'.$shellexec.'</pre>';
 } else {
     // does not contain only letters, numbers, and hyphens
