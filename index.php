8a77834cda849f7856cb6ef3c6e4d70ce1244c1d
Shreejal Maharjan <shreejal.m123@hotmail.com>
2021-08-02 20:37:11 +0545

    strengthed security of the form i guess;- form now posts the data, the options are chosen from an array, etc i dont remember what i did

--- index.php
+++ index.php
@@ -1,29 +1,25 @@
-<html lang="en">
-<head>
 <title>Execute Dig commands</title>
-</head>
-<body>
-<form action="webdig.php" method="GET">
+<form method="post" enctype="multipart/form-data" action="webdig.php" name="form">  
 <input required name="domain" placeholder="enter domain name">
-<input type="checkbox" name="trace" value="+trace">
-<label for="trace">Trace</label>
-<select name="opt1">
-<option value="" disabled selected>Choose connect method</option>
-<option value="-4">IPV4</option>
-<option value="-6">IPV6</option>
-</select>
-<select name="opt2">
-<option value="" disabled selected>Choose records to get</option>
-<option value="ANY +noall +answer">Get All records</option>
-<option value="+nocomments +noquestion +noauthority +noadditional +nostats">Get Answer Only</option>
-<option value="MX +noall +answer">Get MX records</option>
-<option value="NS +noall +answer">Get NS records</option>
-<option value="+nssearch">Get SOA records </option>
-<option value="mx +short">Get Short MX records</option>
-<option value="ns +short">Get Short NS records</option>
-<option value="+short">Get Short Output</option>
-</select>
-<button type="submit">submit</button>
+<select name="ipversion1">
+    <option value="" disabled selected>Choose if trace records</option>
+    <option value="trace">Trace</option>
+ </select>
+<select name="ipversion2">
+    <option value="" disabled selected>Choose Protocol to connect using</option>
+    <option value="v4">IPV4</option>
+    <option value="v6">IPV6</option>
+ </select>
+ <select name="ipversion">
+ <option value="" disabled selected>Choose records to get</option>
+    <option value="getall">Get all records</option>
+    <option value="getansonly">Get Anwers only</option>
+    <option value="getmx">Get MX records</option>
+    <option value="getns">Get NS records</option>
+    <option value="getsoa">Get SOA records</option>
+    <option value="getsmx">Get SHORT MX records</option>
+    <option value="getsns">Get SHORT NS records</option>
+    <option value="getshort">Get SHORT records</option>
+ </select>
+ <input name="submit" type="submit">
 </form>
-</body>
-</html>
--- webdig.php
+++ webdig.php
@@ -1,18 +1,49 @@
 <?php
 //get domain query
-$domain_inp = $_GET["domain"];
-$options_pre1 = $_GET["opt1"];
-$option_pre2 = $_GET["opt2"];
-$trace = $_GET["opt2"];
-$options1 = htmlspecialchars($options_pre1, ENT_QUOTES);
-$options2 = htmlspecialchars($options_pre2, ENT_QUOTES);
-$domain = htmlspecialchars($domain_inp, ENT_QUOTES);
-if (preg_match("/^[.A-Za-z0-9-]+$/", $domain)) {
-        //execute shell command
-        $shellexec = shell_exec("dig $trace $options1 $domain $options2");
-        echo '<pre>'.$shellexec.'</pre>';
-} else {
-    // does not contain only letters, numbers, and hyphens
+$actions = array(      // Array of actions
+    'v4' => '-4',
+    'v6' => '-6',
+    'getall' => 'ANY +noall +answer',
+    'getansonly' => '+nocomments +noquestion +noauthority +noadditional +nostats',
+    'getmx' => 'MX +noall +answer',
+    'getns' => 'NS +noall +answer',
+    'getsoa' => '+nssearch',
+    'getsmx' => 'mx +short',
+    'getsns' => 'ns +short',
+    'getshort' => '+short',
+    'trace' => '+trace'
+);
+if (isset($_POST['ipversion']))
+{   
+    //get domain from input
+
+    $domain = $_POST['domain'];
+    
+    // disallow connection if domain has other than letters, numbers, and hyphens
+    if (preg_match("/^[.A-Za-z0-9-]+$/", $domain)) {
+        if(isset($_POST['ipversion']) && array_key_exists($_POST['ipversion'], $actions)) {
+         if(isset($_POST['ipversion1']) && array_key_exists($_POST['ipversion1'], $actions)) {
+            if(isset($_POST['ipversion1']) && array_key_exists($_POST['ipversion2'], $actions)) {
+            
+                //connect ipv4/v6
+                $ipv = $actions[$_POST['ipversion']];
+                $ipv1 = $actions[$_POST['ipversion1']];
+                $ipv2 = $actions[$_POST['ipversion2']];
+
+                echo "<strong>Command Sample:</strong>";
+                echo "<code> $domain $ipv $ipv1 $ipv2</code>";
+                
+                //execute shell command
+                $shellexec = shell_exec("dig $domain $ipv $ipv1 $ipv2");
+                echo '<pre>'.$shellexec.'</pre>';
+        }
+            } else {
+            die('Unknown Message');  // Otherwise, display and error and end execution
+            }
+        }
+    } else {
+    // display error
     echo 'Invalid domain name provided!';
+    }
 }
 ?>
